9abdaec756c78d47bf9294d3638defc8e15f837f
NgoGiaHuy22DTHG4 <160809116+NgoGiaHuy22DTHG4@users.noreply.github.com>
2024-09-29 19:56:16 +0700

    Add files via upload

---
+++ Bai4FormCuaSONhanVien.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BaiTap4
+{
+    public partial class EmployeeForm : Form
+    {
+        public string MSNV { get; set; }
+        public string TenNhanVien { get; set; }
+        public string LuongCanBan { get; set; }
+        public EmployeeForm()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void EmployeeForm_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnDongY_Click(object sender, EventArgs e)
+        {
+            MSNV = txtMSNV.Text;
+            TenNhanVien = txtTenNhanVien.Text;
+            LuongCanBan = txtLuongCanBan.Text;
+
+            if (string.IsNullOrEmpty(MSNV) || string.IsNullOrEmpty(TenNhanVien) || string.IsNullOrEmpty(LuongCanBan))
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin.");
+                return;
+            }
+            this.DialogResult = DialogResult.OK;
+            this.Close();
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            this.DialogResult = DialogResult.Cancel;
+            this.Close();
+        }
+    }
+}
